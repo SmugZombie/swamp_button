b8242e7b4a4d7a40c2fc03f200ed9011427acc83
Ron Egli <ron@r-egli.com>
2018-07-13 08:59:52 +0000

    Added comments, cleaned code

--- swamp_iot_button.py
+++ swamp_iot_button.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python
-import thread
-import RPi.GPIO as GPIO
-import time, requests, json, logging
+# Swamp IoT Button Service
+# Ron Egli / Github.com/SmugZombie
+# Simple script to listen for a button imput to update an api and activate LEDS
+# Version 1.1
 
+import RPi.GPIO as GPIO
+import time, requests, json, logging, thread
+
+# Define Global Variables
 lightState=0
+
+# Setup GPIO for our uses
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setwarnings(False)
@@ -11,11 +18,13 @@ GPIO.setup(18,GPIO.OUT) # Red LED
 GPIO.setup(17,GPIO.OUT) # Green LED
 GPIO.setup(16,GPIO.OUT) # Yellow LED
 
-# Light Up Yellow
+# Light Up Yellow to signify "ON"
 GPIO.output(16,GPIO.HIGH)
 
+# Setup Logger
 logging.basicConfig(filename='runtime.log',level=logging.DEBUG)
 
+# Retrieves the current status from the api and sets the light that color
 def getAPIState():
     global lightState
     url = "http://localhost/api/index.php"
@@ -27,6 +36,7 @@ def getAPIState():
     logging.debug(lightState)
     toggleLights()
 
+# Toggles the color of the light
 def toggleLights():
     global lightState
     if lightState == 1:
@@ -38,6 +48,17 @@ def toggleLights():
         GPIO.output(17,GPIO.HIGH)
         GPIO.output(18,GPIO.LOW)
 
+# Toggles the status of the light after accepting button input via the API
+def toggleStatus():
+    global lightState
+    url = "http://localhost/api/index.php"
+    querystring = {"action":"toggle"}
+    headers = {}
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    logging.debug(response.text)
+    lightState = int(json.loads(response.text)['status'])
+
+# Service to check the API at given interval, Recommended no faster than 1 second
 def runAPIChecker(thread_name):
     logging.debug("Starting API Checker")
     try:
@@ -47,21 +68,10 @@ def runAPIChecker(thread_name):
     except:
         GPIO.cleanup()
         logging.debug("Killing API Checker")
-    #    GPIO.output(18,GPIO.LOW)
-    #    GPIO.output(17,GPIO.LOW)
-    #    GPIO.output(16,GPIO.HIGH)
 
-
+# Service to check if the button is being pressed. Recommended no faster than 0.1 seconds
 def runButtonListener(thread_name):
     logging.debug("Starting Button Listener")
-    def toggleStatus():
-        global lightState
-        url = "http://localhost/api/index.php"
-        querystring = {"action":"toggle"}
-        headers = {}
-        response = requests.request("GET", url, headers=headers, params=querystring)
-        logging.debug(response.text)
-        lightState = int(json.loads(response.text)['status'])
 
     try:
         while True:
@@ -75,11 +85,10 @@ def runButtonListener(thread_name):
         GPIO.cleanup()
         logging.debug("Killing Button Listener")
 
-# Create two threads as follows
+# Handles the magic of threading
 try:
    thread.start_new_thread( runAPIChecker, ("Thread-1", ) )
    thread.start_new_thread( runButtonListener, ("Thread-2", ) )
-   #thread.start_new_thread( print_time, ("Thread-2", 4, ) )
 except:
    print "Error: unable to start thread"
 
